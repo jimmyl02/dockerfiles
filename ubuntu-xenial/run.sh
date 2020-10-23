faa23c0b95549a62ffb0b8aeea8eeaa3dc9be30a
jimmyl02 <jimmyli2002@gmail.com>
2020-10-23 01:47:38 +0000

    updated ubunutu-xenial with pwntools and one_gadget

--- ubuntu-xenial/run.sh
+++ ubuntu-xenial/run.sh
@@ -1,1 +1,1 @@
-docker run -it --cap-add sys_ptrace --security-opt seccomp=unconfined -v $(pwd):/pwn pwn:xenial
+docker run -it --privileged --pid=host --cap-add sys_ptrace --security-opt seccomp=unconfined --security-opt apparmor=unconfined -v $(pwd):/pwn pwn:xenial
